e8d1932b59bb37ec395ae6935b8bae383447ceab
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-12-20 16:33:50 +0100

    Fix indent

--- api/module.h
+++ api/module.h
@@ -3,25 +3,27 @@
 #include "conf.h"
 #include "http.h"
 
-namespace zia::api {
-	/**
-	 * Interface for modules. Functions must be safe in a multithreading context.
-	 * Dynamic libraries of modules must export a "create" C function returning a "Module*" (caller should use smart pointers).
-	 */
-	class Module {
-	public:
-		virtual ~Module() = default;
+namespace zia::api
+{
+    /**
+     * Interface for modules. Functions must be safe in a multithreading context.
+     * Dynamic libraries of modules must export a "create" C function returning a "Module*" (caller should use smart pointers).
+     */
+    class Module
+    {
+    public:
+        virtual ~Module() = default;
 
-		/**
-		* Called after contruct and when config changed.
-		* \return true on success, otherwise false.
-		*/
-		virtual bool config(const Conf& conf) = 0;
+        /**
+        * Called after contruct and when config changed.
+        * \return true on success, otherwise false.
+        */
+        virtual bool config(const Conf &conf) = 0;
 
-		/**
-		* Called on HTTP request.
-		* \return true on success, otherwise false.
-		*/
-		virtual bool exec(HttpDuplex& http) = 0;
-	};
+        /**
+        * Called on HTTP request.
+        * \return true on success, otherwise false.
+        */
+        virtual bool exec(HttpDuplex &http) = 0;
+    };
 }
